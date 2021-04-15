bb9295235a6b563ec4a85473232e97aa21bf5bfe
nix18 <kyj20167@gmail.com>
2021-04-15 16:56:13 +0800

    20210415 submit 完善习惯购买页面前端部分

--- app/src/main/java/com/myapp/gradutest_android/Habit_Info_Activity.java
+++ app/src/main/java/com/myapp/gradutest_android/Habit_Info_Activity.java
@@ -1,5 +1,6 @@
 package com.myapp.gradutest_android;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,11 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
 import com.bigkoo.pickerview.builder.TimePickerBuilder;
+import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
 import com.bigkoo.pickerview.listener.OnTimeSelectListener;
+import com.bigkoo.pickerview.view.OptionsPickerView;
 import com.bigkoo.pickerview.view.TimePickerView;
 import com.myapp.gradutest_android.domain.GoodHabit;
 import com.myapp.gradutest_android.utils.habit.habitUtils;
@@ -32,6 +36,7 @@ public class Habit_Info_Activity extends AppCompatActivity {
     private ButtonView start_time_btn;
     private ButtonView end_time_btn;
     private ButtonView remind_time_btn;
+    private ButtonView credit_in_btn;
     private GoodHabit thisHabit;
 
     @Override
@@ -45,6 +50,9 @@ public class Habit_Info_Activity extends AppCompatActivity {
         String nowTime = new SimpleDateFormat("a HH:mm").format(Calendar.getInstance().getTime());
         habit_name_text.setText(thisHabit.getHabit_name());
         start_time_btn.start_text.setText("开始日期");
+        end_time_btn.start_text.setText("结束日期");
+        remind_time_btn.start_text.setText("提醒时间");
+        credit_in_btn.start_text.setText("投入积分");
         start_time_btn.end_text.setText(now);
         end_time_btn.end_text.setText(now);
         remind_time_btn.end_text.setText(nowTime);
@@ -97,8 +105,53 @@ public class Habit_Info_Activity extends AppCompatActivity {
                 pvTime.show();
             }
         });
-        end_time_btn.start_text.setText("结束日期");
-        remind_time_btn.start_text.setText("提醒时间");
+
+        //生成积分梯度
+        ArrayList<Integer> credit_sel = new ArrayList<>();
+        int i = 1;
+        while (i < 50){
+            credit_sel.add(i * 10);
+            i++;
+        }
+        i = 5;
+        while (i < 100){
+            credit_sel.add(i * 100);
+            i++;
+        }
+
+        credit_in_btn.end_text.setText("预计返还 "+"75"+" 积分");
+        final int[] savedOption = {4};
+        credit_in_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OptionsPickerView pvNoLinkOptions = new OptionsPickerBuilder(Habit_Info_Activity.this, new OnOptionsSelectListener() {
+
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onOptionsSelect(int options1, int options2, int options3, View v) {
+                        double multiple = 1;
+                        if(credit_sel.get(options1)<100){
+                            multiple = 1.5;
+                        }else if(credit_sel.get(options1)<300){
+                            multiple = 2;
+                        }else if(credit_sel.get(options1)<600){
+                            multiple = 2.5;
+                        }else {
+                            multiple = 3;
+                        }
+                        credit_in_btn.end_text.setText("预计返还 "+((int)(credit_sel.get(options1)*multiple))+" 积分");
+                        savedOption[0] = options1;
+                    }
+                })
+                        .setSubmitColor(getColor(R.color.orange))
+                        .setCancelColor(getColor(R.color.orange))
+                        .setItemVisibleCount(6)
+                        .build();
+                pvNoLinkOptions.setPicker(credit_sel);
+                pvNoLinkOptions.setSelectOptions(savedOption[0]);
+                pvNoLinkOptions.show();
+            }
+        });
         back_btn.setOnClickListener(v -> finish());
         more_info_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -116,6 +169,7 @@ public class Habit_Info_Activity extends AppCompatActivity {
         start_time_btn = findViewById(R.id.start_time_habit_info);
         end_time_btn = findViewById(R.id.end_time_habit_info);
         remind_time_btn = findViewById(R.id.remind_time_habit_info);
+        credit_in_btn = findViewById(R.id.credit_in_btn_habit_info);
     }
 
     public void initData(){
